97450f8cad02f98a9bee56fe8ad2e415b6be60dc
sadiaazdaou <sadia.azdaou@gmail.com>
2025-10-04 07:45:36 -0400

    create Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-
-import './App.css'
-import HomePage from './pages/HomePage'
+import { Routes, Route } from "react-router";
+import "./App.css";
+import HomePage from "./pages/HomePage";
 
 function App() {
- 
-
-  return (
-    <>
-    <HomePage />
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route index element={<HomePage />} />
+                <Route
+                    path="/checkout"
+                    element={
+                        <div
+                            style={{
+                                color: "red",
+                                backgroundColor: "blue",
+                                zIndex: 99,
+                            }}>
+                            Test Checkout Page
+                        </div>
+                    } />
+            </Routes>
+            <HomePage />
+        </>
+    );
 }
 
-export default App
+export default App;
