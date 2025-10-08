b2aa3a3d653fc969fb97a61c87651dd051119095
sadiaazdaou <sadia.azdaou@gmail.com>
2025-10-08 10:19:00 -0400

    Add test hook 'beforeEach'

--- src/pages/home/Product.test.jsx
+++ src/pages/home/Product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi } from "vitest";
+import { it, expect, describe, vi, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Product } from "./Product";
@@ -7,19 +7,28 @@ import axios from "axios";
 vi.mock("axios");
 
 describe("Product Component", () => {
+    let product 
+    let loadCart 
+
+  //beforeEach is a Test Hook as afterEach(), beforeAll(), afterAll()
+  beforeEach(() => { 
+product = {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+        stars: 4.5,
+        count: 87,
+    },
+    priceCents: 1090,
+    keywords: ["socks", "sports", "apparel"],
+    };
+    
+    loadCart = vi.fn();
+  })
+  
     it("Display the products details correctly", () => {
-        const product = {
-            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-            rating: {
-                stars: 4.5,
-                count: 87,
-            },
-            priceCents: 1090,
-            keywords: ["socks", "sports", "apparel"],
-        };
-        const loadCart = vi.fn();
+      
 
         render(<Product product={product} loadCart={loadCart} />);
 
@@ -36,18 +45,7 @@ describe("Product Component", () => {
     });
 
     it("Add product to the cart", async () => {
-        const product = {
-            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-            rating: {
-                stars: 4.5,
-                count: 87,
-            },
-            priceCents: 1090,
-            keywords: ["socks", "sports", "apparel"],
-        };
-        const loadCart = vi.fn();
+       
 
         render(<Product product={product} loadCart={loadCart} />);
 
@@ -59,6 +57,6 @@ describe("Product Component", () => {
             productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
             quantity: 1,
         });
-        
+        //expect(loadCart).toHaveBeenCalled();
     });
 });
